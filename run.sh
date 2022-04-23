9d3767401debb225e17223d79b22d847481447f7
Aaron Stump <aaron-stump@uiowa.edu>
2022-04-22 23:01:49 -0500

    script to run hidden tests (none yet)

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "======================================================"
+echo "Running hidden tests"
+echo ""
+echo "No hidden tests yet. They will be added after the"
+echo "deadline to help the staff with grading."
+echo "======================================================"

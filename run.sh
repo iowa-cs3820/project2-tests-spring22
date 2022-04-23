315550b7bd95807a1933dc80fb68e7bb557ab990
Aaron Stump <aaron-stump@uiowa.edu>
2022-04-23 16:56:48 -0500

    exit 1

--- run.sh
+++ run.sh
@@ -6,3 +6,6 @@ echo ""
 echo "No hidden tests yet. They will be added after the"
 echo "deadline to help the staff with grading."
 echo "======================================================"
+
+# just to signal that there are no points being given yet for this
+exit 1
